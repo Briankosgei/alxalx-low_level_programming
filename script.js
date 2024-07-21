f00ede67e59b136d37cf9765eebb4592961b272c
Briankosgei <127298330+Briankosgei@users.noreply.github.com>
2024-07-21 15:24:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
 const signupButton=document.getElementById("signupButton")
 const SignIn=document.getElementById("SignIn")
-const SignInform=document.getElementById("SignIn")
-const signupform=document.getElementById("SignUp")
-
-signupButton.addEventListener('click'), function(){
-    signupform.style.display="none";
-    signupButton.style.display="block";
-
-}
-SignIn.addEventListener('click'), function(){
-    signInform.style.display="block";
-    signupButton.style.display="none";
-}
